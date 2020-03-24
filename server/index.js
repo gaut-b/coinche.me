55ad78549c0c089ae015effada5eee20d7160b51
Augustin Riedinger <augustin.riedinger@gmail.com>
2020-03-24 17:45:59 +0100

    better project structure with command

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const app = express()
 app.use(express.static('build'));
 app.use(express.static('public'));
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!')
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Le coincheur listening on port ${PORT}!`)
 })
