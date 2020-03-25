91ee2d150b16dc2f8d12755f4bebca2355c42228
gaut-b <gautier.bayle@gmail.com>
2020-03-25 14:08:10 +0100

    add collect action

--- app/js/redux/actions.js
+++ app/js/redux/actions.js
@@ -1,15 +1,23 @@
 export const ACTIONS = {
   DISTRIBUTE: 'DISTRIBUTE',
   PLAY_CARD: 'PLAY_CARD',
+  COLLECT: 'COLLECT',
 };
 
 export function distribute() {
   return { type: ACTIONS.DISTRIBUTE }
-}
+};
 
 export function playCard(card) {
   return {
     type: ACTIONS.PLAY_CARD,
     payload: card,
   }
-}
+};
+
+export function collect(cards) {
+  return {
+    type: ACTIONS.COLLECT,
+    payload: cards
+  }
+};
