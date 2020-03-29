e172d8529a8afaff678e4979d3c45f8236e752cf
gaut-b <gautier.bayle@gmail.com>
2020-03-29 15:56:35 +0200

    Write subjectiveState function

--- server/redux/subjectiveState.js
+++ server/redux/subjectiveState.js
@@ -1,3 +1,22 @@
-export default function subjectiveState(store, id) {
-  return store.getState();
-}
+import import {NORTH, EAST, SOUTH, WEST} from '../../shared/constants/positions';
+
+export default function subjectiveState(store, playerId) {
+  const state = store.getState();
+
+  const playerIndex = state.players.findIndex(p => p.id === playerId)
+
+  if (playerIndex === -1) return state;
+
+  const updatedPlayer = state.players.map((p, i) => {
+    if (i === playerIndex) p.position = SOUTH;
+    else if (i === playerIndex + 1 % 4) p.position = WEST;
+    else if (i === playerIndex + 2 % 4) p.position = NORTH;
+    else if (i === playerIndex + 3 % 4) p.position = EAST;
+    return p;
+  });
+
+  return {
+    ...state,
+    players: updatedPlayer,
+  };
+};
