b8561b1be6dc31aeaa575ad9ed6fc48eb72ffeeb
gaut-b <gautier.bayle@gmail.com>
2020-04-03 09:26:01 +0200

    Move button to Table component and add some css

--- app/js/components/Table.js
+++ app/js/components/Table.js
@@ -1,6 +1,6 @@
 import React, { Component, useContext } from 'react';
 import { connect } from 'react-redux';
-import { collect } from '../redux/actions';
+import { collect, undo } from '../redux/actions';
 import PropTypes from 'prop-types';
 import {TableIdContext} from '../pages/GamePage';
 import {selectCurrentPlayer, selectCanCollect, selectOnTable} from '../redux/selectors'
@@ -8,7 +8,7 @@ import {selectCurrentPlayer, selectCanCollect, selectOnTable} from '../redux/sel
 import '../../scss/components/table.scss';
 import Card from './Card';
 
-const Table = ({onTable, currentPlayer, collect, canCollect}) => {
+const Table = ({onTable, currentPlayer, collect, undo, canCollect}) => {
 
   const tableId = useContext(TableIdContext);
 
@@ -20,6 +20,7 @@ const Table = ({onTable, currentPlayer, collect, canCollect}) => {
   return (
     <div className="playing-table">
       {canCollect && <button onClick={(e) => handleClick(e)} className="button is-primary is-collect">Ramasser</button>}
+      <button className={`button is-primary ${!canCollect ? 'is-alone' : 'is-not-alone'}`} onClick={() => undo(tableId)} style={{marginTop: '2rem'}}>Undo</button>
       {onTable.map(({position, value}, index) => {
         return (
           <div key={value} className={`table-slot is-${position}`} style={{zIndex: index}}>
@@ -43,6 +44,7 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = (dispatch) => ({
   collect: (tableId, index) => dispatch(collect(tableId, index)),
+  undo: (tableId) => dispatch(undo(tableId)),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Table);
