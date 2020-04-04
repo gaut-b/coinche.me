4cb9b7b9800f89cf39554fb9a58e3bdf06c035b5
Augustin Riedinger <augustin.riedinger@gmail.com>
2020-04-04 17:40:04 +0200

    trying to debug

--- app/js/pages/GamePage.js
+++ app/js/pages/GamePage.js
@@ -1,18 +1,20 @@
 import React, { useState, createContext } from 'react';
-import { Redirect } from "react-router-dom";
+import { Redirect } from 'react-router-dom';
 import Layout from '../components/Layout';
 import Game from '../components/Game';
 
 export const TableIdContext = createContext('');
 
 const GamePage = ({match: {params: {tableId}}}) => {
-  const [state, setState] = useState(tableId);
+  // const [state, setState] = useState(tableId);
 
-  if (!state) return (
+  console.log(tableId);
+
+  if (!tableId) return (
     <Redirect to="/" />
   );
   return (
-    <TableIdContext.Provider value={state}>
+    <TableIdContext.Provider value={tableId}>
       <Layout>
         <Game />
       </Layout>
